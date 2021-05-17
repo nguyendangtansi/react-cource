da5bdafbcd85cd7af2de39f42500db935ede0ad7
nguyendangtansi <nguyendangtansi90@gmail.com>
2021-05-17 12:44:19 -0700

    f commit

---
+++ part1/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}. You are {props.age} years old</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return 
+    <>
+    <h1>Greetings</h1>
+    <Hello name="Maya" age={26 + 10} />
+    <Footer />
+  </>
+}
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
+    </div>
+  )
+}
+
+export default App
